d87393505e7824ffeead2c5e5e5a4bc2c8bb7cfa
Mukai Shiru <shiru@duck.com>
2022-09-19 21:27:42 +0900

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by モクタリ・シルヴァン on 2022/09/19.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
